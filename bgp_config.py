fd6e877a27d33e2acb75754ed532d2381ffd715c
Daniel Hertzberg <burnyd@gmail.com>
2019-03-12 19:54:12 -0400

    made changes to bgp_config.py

--- bgp_config.py
+++ bgp_config.py
@@ -10,19 +10,19 @@ import ocbind
 from pyangbind.lib.serialise import pybindJSONDecoder
 ph = xpathhelper.YANGPathHelper()
 
-eos_bgp = {'ceos1': {'AS': '65001', 'PEER_AS': '65002', 'ROUTER_ID': '1.1.1.1', 'PEER': '12.12.12.2'}, 'ceos2': {'AS': '65002', 'PEER_AS': '65001', 'ROUTER_ID': '2.2.2.2', 'PEER': '12.12.12.22'}}
+eos_bgp = {'ceos1': {'AS': '65001', 'PEER_AS': '65002', 'ROUTER_ID': '1.1.1.1', 'PEER': 12.12.12.2}, 'ceos2': {'AS': '65002', 'PEER_AS': '65001', 'ROUTER_ID': '2.2.2.2', 'PEER': 12.12.12.1}}
 
 
-func main():
-for bgp_results in eos_bgp:
+def main():
+for k,v in eos_bgp.iteritems():
     ocbgp = ocbind.openconfig_bgp(path_helper=ph)
-    ocbgp.bgp.global_.config.as_ = bgp_results['AS']
-    ocbgp.bgp.global_.config.router_id = bgp_results['ROUTER_ID']
-    ocbgp.bgp.neighbors.neighbor.add(bgp_results['PEER'])
-    ocbgp.bgp.neighbors.neighbor[bgp_results['PEER']].config.peer_as = bgp_results['PEER_AS']
-    struct =  json.dumps(ocbgp.get(filter=True), indent=4)
+    ocbgp.bgp.global_.config.as_ = v['AS']
+    ocbgp.bgp.global_.config.router_id = v['ROUTER_ID']
+    ocbgp.bgp.neighbors.neighbor.add(str(v['PEER']))
+    ocbgp.bgp.neighbors.neighbor[str(v['PEER'])].config.peer_as = v['PEER_AS']
+    struct = json.dumps(json.loads(pybindJSON.dumps(ocbgp)), indent=4, sort_keys=True)
     print struct
-    with open(bgp_results, 'w') as f:
+    with open(k, 'w') as f:
       f.write(struct)
 
 main()
