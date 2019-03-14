6e8f845e2a03915cb27d68d8f146a3b959f5b812
Daniel Hertzberg <burnyd@gmail.com>
2019-03-14 17:00:32 -0400

    added source

--- scripts/source.sh
+++ scripts/source.sh
@@ -1,9 +1,9 @@
 echo "Installing go and setting env variables for Go"
-wget https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz -P /opt/
+wget https://storage.googleapis.com/golang/go1.11.1.linux-amd64.tar.gz -P /opt/
 
 sleep 3
 
-tar -C /usr/local -xzf /opt/go1.9.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf /opt/go1.11.1.linux-amd64.tar.gz
 echo Adding Golang variables...
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$(go env GOPATH)/bin
